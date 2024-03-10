25d5f243625926649df677d6301bf74f36909c25
shaky008 <sshakya.uutsav@gmail.com>
2024-03-09 21:07:08 -0800

    added Add component

--- frontend/src/pages/Add.jsx
+++ frontend/src/pages/Add.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Add = () => {
-  return <div>Add</div>;
+  const [note, setNote] = useState({
+    title: "",
+    desc: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setNote((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    //prevents browser from reloading when clicking button
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8800/notes", note);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="add">
+      <h1>Add Notes</h1>
+      <input
+        type="text"
+        placeholder="title"
+        name="title"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        placeholder="descption"
+        name="desc"
+        onChange={handleChange}
+      />
+      <button onClick={handleClick}>Upload</button>
+    </div>
+  );
 };
 
 export default Add;
